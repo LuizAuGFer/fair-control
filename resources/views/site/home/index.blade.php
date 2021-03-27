f7226de7faaa6d988a2b7d88462bd9f9b1e9f276
LuizAuGFer <luiz.augusto9922@outlook.com>
2021-03-27 12:16:45 -0300

    Iniciando criação das migrations

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.site')
 @section('content')
-<div class="container">
-
+<div class="container mt-4">
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Vender</h5>
+        <p class="card-text">Realizar uma venda</p>
+        <a href="#" class="btn btn-primary">Vender</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Comprar</h5>
+        <p class="card-text">Comprar </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 mt-1">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Clientes</h5>
+        <p class="card-text">Comprar </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
 @endsection
 
